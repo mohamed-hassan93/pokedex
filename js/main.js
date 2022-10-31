4756f002656c6e29e62c9a0b4e8cca52740df509
Mohamed <42161293+mohamed-hassan93@users.noreply.github.com>
2022-10-31 02:09:02 -0400

    pokedex fully functional - assessibility concerns

--- js/main.js
+++ js/main.js
@@ -19,13 +19,47 @@ document.querySelector('.search').addEventListener('click', runPokeDex);
 
 function runPokeDex(){
  
-    let pokemon = document.getElementById('pokemonID')
+    let pokemon = document.querySelector('#pokemonID').value.toLowerCase();
+    let pokeUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    let randomPPOne = randomPPGenerator()
+    let randomPPTwo = randomPPGenerator()
+    let randomPPThree = randomPPGenerator()
+    let randomPPFour = randomPPGenerator()
+
+    function randomPPGenerator(){
+        let value = Math.ceil(Math.random()*20)
+        return value
+    }
+
+    function randomMoveGenerator(){
+        let value = Math.ceil(Math.random()*70)
+        return value 
+    }
  
-    fetch(apiUrl)
+    fetch(pokeUrl)
     .then(res => res.json())
   
     .then(data => {
         console.log(data);
+
+        document.querySelector('.pokeImageSrc').src = data.sprites.front_default;
+        document.querySelector('.pokeData1Title').innerHTML = data.species.name.toUpperCase();
+        document.querySelector('.pokeData1ItemOne').innerHTML = `Type: ${data.types[0].type.name.toUpperCase()}`;
+        document.querySelector('.pokeData1ItemTwo').innerHTML = `Height: ${data.height*10}" `;
+        document.querySelector('.pokeData1ItemThree').innerHTML = `Weight: ${data.weight} lbs`;
+        document.querySelector('.pokeData1ItemFour').innerHTML = `Pokedex ID: ${data.id}`;
+
+        document.querySelector('.abilityOne').innerHTML = data.moves[randomMoveGenerator()].move.name;
+        document.querySelector('.abilityTwo').innerHTML = data.moves[randomMoveGenerator()].move.name;
+        document.querySelector('.abilityThree').innerHTML = data.moves[randomMoveGenerator()].move.name;
+        document.querySelector('.abilityFour').innerHTML = data.moves[randomMoveGenerator()].move.name;
+
+        
+        document.querySelector('.abilityOnePP').innerHTML = `PP: ${randomPPOne}/${randomPPOne}`
+        document.querySelector('.abilityTwoPP').innerHTML = `PP: ${randomPPTwo}/${randomPPTwo}`
+        document.querySelector('.abilityThreePP').innerHTML = `PP: ${randomPPThree}/${randomPPThree}`
+        document.querySelector('.abilityFourPP').innerHTML = `PP: ${randomPPFour}/${randomPPFour}`
+
      })
     .catch(err => {
         console.log(`error ${err}`)

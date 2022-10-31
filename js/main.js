2957d1181b56efff9f2522f3b0f10d93929c57af
Mohamed <42161293+mohamed-hassan93@users.noreply.github.com>
2022-10-30 21:22:36 -0700

    updated js file

--- js/main.js
+++ js/main.js
@@ -13,3 +13,21 @@ function play() {
     var audio = document.getElementById("audio");
     audio.play();
   }
+
+
+document.querySelector('.search').addEventListener('click', runPokeDex);
+
+function runPokeDex(){
+ 
+    let pokemon = document.getElementById('pokemonID')
+ 
+    fetch(apiUrl)
+    .then(res => res.json())
+  
+    .then(data => {
+        console.log(data);
+     })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+}
